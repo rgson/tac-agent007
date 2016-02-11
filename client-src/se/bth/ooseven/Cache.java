8132b6dc5a4dca091288f298dffa487bf6344799
Tamme Dittrich <tdittr@technik-emden.de>
2016-02-11 22:25:54 +0100

    Created a fake-cache that always runs the FastOptimizer.
    This way you get real values, and don't need to wait so much for my Implementation.

--- client-src/se/bth/ooseven/Cache.java
+++ client-src/se/bth/ooseven/Cache.java
@@ -1,11 +1,17 @@
 package se.bth.ooseven;
 
+import se.sics.tac.solver.FastOptimizer;
+
 public class Cache {
+    private final Preferences prefs;
     public Cache(Preferences prefs) {
-    
+        this.prefs = prefs;
     }
     
     public int calc(Owns owns) {
-        return 42;
+        FastOptimizer fo = new FastOptimizer();
+        fo.setClientData(prefs.getSolverFormat(), owns.getSolverFormat());
+    
+        return fo.solve();
     }
 }
