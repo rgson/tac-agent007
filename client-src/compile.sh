ceda5f04bf4e2d7007c5a78b575b1326f5bd32e5
Robin Gustafsson <robingustafsson93@gmail.com>
2016-02-25 18:42:14 +0100

    Modified compile scripts to include our code in the jar

--- client-src/compile.sh
+++ client-src/compile.sh
@@ -3,4 +3,4 @@ javac -classpath . se/sics/tac/util/*.java
 javac -classpath . se/sics/tac/aw/*.java
 javac -classpath . se/bth/ooseven/*.java
 
-jar cfm tacagent.jar AWManifest.txt com/botbox/util/*.class se/sics/tac/aw/*.class se/sics/tac/util/*.class
+jar cfm tacagent.jar AWManifest.txt com/botbox/util/*.class se/sics/tac/aw/*.class se/sics/tac/util/*.class se/bth/ooseven/*.class
